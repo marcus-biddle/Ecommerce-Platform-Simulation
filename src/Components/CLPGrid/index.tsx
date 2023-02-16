6f08bf8eeeb1cf02b6eb6ac4f07a638ca2b8fddc
marcus-biddle <marcusbiddle16@gmail.com>
2023-02-16 15:45:46 -0800

    api no longer calls twice

--- src/Components/CLPGrid/index.tsx
+++ src/Components/CLPGrid/index.tsx
@@ -3,48 +3,44 @@ import React, { useEffect, useState } from 'react'
 import { REGIONS } from '../../constants';
 
 export const CLPGrid = () => {
-    // Will need to grab regional pokemon and their images. As well as figure out pricing
+   // We can move the api call to context to save the state then be able to use the data anywhere
     const [pokemonNames, setPokemonNames] = useState<any[]>([]);
     const [isLoading, setLoading] = useState(true);
 
-        useEffect(() => {
-            const pokemonNamesClient = axios.create({
-                baseURL: "https://pokeapi.co/api/v2/pokemon/"
-            })
-        
-            const fetchPokemonNames = () => {
-                pokemonNamesClient.get(`?limit=${REGIONS[0].value.last}&offset=${REGIONS[0].value.first}`) // include limit and offset to get specific region
-                .then((response) => {
-                    const requestPokemon = [];
+    const pokemonNamesClient = axios.create({
+        baseURL: "https://pokeapi.co/api/v2/pokemon/"
+    })
 
-                    for (let pokemon of response.data.results) {
-                        const url = pokemon.url;
-                        requestPokemon.push(axios.get(`${url}`));
-                    }
+    const fetchPokemonNames = async() => {
+        pokemonNamesClient.get(`?limit=${REGIONS[0].value.last}&offset=${REGIONS[0].value.first}`) // include limit and offset to get specific region
+        .then((response) => {
 
-                    return Promise.all(requestPokemon);
-                    // setPokemonNames(response.data.results)
-                    // setLoading(false);
-                })
-                .then((responseList) => {
-                    for (let response of responseList) {
-                        setPokemonNames((curr) => [...curr, response.data])
-                    }
-                })
+            const requestPokemon = [];
+
+            for (let pokemon of response.data.results) {
+                const url = pokemon.url;
+                requestPokemon.push(axios.get(`${url}`));
             }
+            return Promise.all(requestPokemon);
+        })
+        .then((responseList) => {
 
+            const dataList = [];
+            
+            for (let response of responseList) {
+                dataList.push(response.data);
+            }
+            setPokemonNames(dataList)
+            setLoading(false);
+
+        })
+    }
+        useEffect(() => {
             fetchPokemonNames();
         }, [])
 
-        // useEffect(() => {
-        //     pokemonNames.forEach((pokemon) => {
+        console.log(pokemonNames);
 
-        //         const pokemonClient = axios.create({
-        //             baseURL: ``
-        //         })
-        //     })
-            
-        // })
 
     return (
     <div>
